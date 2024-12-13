5148e3a812677a986a0f7c2397d7231e7018930b
Mac <mac@Macs-MacBook-Pro.local>
2024-12-13 16:37:05 -0400

    add pods & terraform

---
+++ login.tf
@@ -0,0 +1,20 @@
+provider "kubernetes" {
+  config_context_auth_info = "k8s-admin@kubernetes"
+}
+
+resource "kubernetes_namespace" "devops" {
+  metadata {
+    name = "devops"
+  }
+}
+
+output "kubeconfig" {
+  value = data.kubernetes_secret.admin.data["kubeconfig"]
+}
+
+data "kubernetes_secret" "admin" {
+  metadata {
+    name      = "k8s-admin"
+    namespace = "kube-system"
+  }
+}
